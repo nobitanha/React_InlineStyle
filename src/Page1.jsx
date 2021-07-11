0ca8f91610c3cdb7c918a7d2dc9a2c6f84534e52
nobitanha <44314450+nobitanha@users.noreply.github.com>
2021-07-11 16:48:38 +0900

    Truyen parameter(information) cho page Doi voi the Link, sau "to=" co the truyen mot object javascript: {{ pathname -> path state -> parameter truyen cho page(ko the truyen qua link vi no qua lon) }}

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 
 export const Page1 = () => {
+  const arr = [...Array(100).keys()];
+  console.log(arr);
   return (
     <div>
       <h1> Page1ページです。</h1>
-      <Link to="/page1/detailA">Detail A</Link>
+      <Link to={{ pathname: "/page1/detailA", state: arr }}>Detail A</Link>
       <br />
       <Link to="/page1/detailB">Detail B</Link>
     </div>
