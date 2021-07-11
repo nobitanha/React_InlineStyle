beff42141f93ab3ba8666880fef55f65bf9d71d9
nobitanha <44314450+nobitanha@users.noreply.github.com>
2021-07-11 16:01:42 +0900

    Phan hoach lai dinh nghia cua cac Route tag -> Define mot file jsx chua mot map dinh nghia thong tin cua Route. O day la Page1DetailXXX

--- src/router/Page1Routes.jsx
+++ src/router/Page1Routes.jsx
@@ -2,7 +2,7 @@ import { Page1 } from "../Page1";
 import { Page1DetailA } from "../Page1DetailA";
 import { Page1DetailB } from "../Page1DetailB";
 
-export const Page1Routers = [
+export const page1Routers = [
   {
     path: "",
     exact: true,
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,9 +1,7 @@
 import { Route, Switch } from "react-router-dom";
 import { Home } from "../Home";
-import { Page1 } from "../Page1";
-import { Page1DetailA } from "../Page1DetailA";
-import { Page1DetailB } from "../Page1DetailB";
 import { Page2 } from "../Page2";
+import { page1Routers } from "./Page1Routes";
 
 export const Router = () => {
   return (
@@ -15,16 +13,15 @@ export const Router = () => {
         path="/page1"
         render={({ match: { url } }) => (
           <Switch>
-            {console.log(url)}
-            <Route exact path={url}>
-              <Page1 />
-            </Route>
-            <Route exact path={`${url}/detailA`}>
-              <Page1DetailA />
-            </Route>
-            <Route exact path={`${url}/detailB`}>
-              <Page1DetailB />
-            </Route>
+            {page1Routers.map((route) => (
+              <Route
+                key={route.path}
+                exact={route.exact}
+                path={`${url}${route.path}`}
+              >
+                {route.children}
+              </Route>
+            ))}
           </Switch>
         )}
       ></Route>
