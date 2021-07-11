d4e50f0580356bc221a15f74fb9c280432b1a7a0
nobitanha <44314450+nobitanha@users.noreply.github.com>
2021-07-11 17:21:19 +0900

    Page transition using javascript + Import useHistory library -> khai bao : const history = useHistory(); + history.push("XXX"): XXX la path cua trang web moi + history.goBack(): tro ve trang truoc do

--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,11 +1,15 @@
-import { useLocation } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 
 export const Page1DetailA = () => {
   const location = useLocation;
   console.log(location);
+  const history = useHistory();
+
+  const onClickBack = () => history.goBack();
   return (
     <div>
       <h1> Page1 Detail A ページです。</h1>
+      <button onClick={onClickBack}>戻る</button>
     </div>
   );
 };
