f34b623eff0d35eb08c1343eaf93bb39508a9263
nobitanha <44314450+nobitanha@users.noreply.github.com>
2021-07-12 23:36:23 +0900

    initial commit

---
+++ src/components/atoms/button/BaseButton.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const BaseButton = styled.button`
+  background-color: #40514e;
+  color: #fff;
+  padding: 12px 24px;
+  border: none;
+  outline: none;
+  border-radius: 9999px;
+  &:hover {
+    cursor: pointer;
+    opacity: 0.8;
+  }
+`;
