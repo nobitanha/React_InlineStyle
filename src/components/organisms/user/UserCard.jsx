18454455281cbd4207509637fbea553c0ea8040d
nobitanha <44314450+nobitanha@users.noreply.github.com>
2021-07-18 13:33:01 +0900

    Create Organism 1 Create UserCard Organism

---
+++ src/components/organisms/user/UserCard.jsx
@@ -0,0 +1,18 @@
+export const UserCard = () => {
+  return (
+    <div>
+      <img alt="プロフィール" />
+      <p>名前</p>
+      <dl>
+        <dt>メール</dt>
+        <dd>asfaf@hay.com</dd>
+        <dt>TEL</dt>
+        <dd>1234-4567-8902</dd>
+        <dt>会社名</dt>
+        <dd>ああああああ</dd>
+        <dt>WEB</dt>
+        <dd>aasdfadsf.com</dd>
+      </dl>
+    </div>
+  );
+};
